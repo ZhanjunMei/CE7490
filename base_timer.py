295df0c49945b5ff45f27cccdeead46a1608c442
mtr329 <793562654@qq.com>
2024-11-04 17:38:18 +0800

    structure

---
+++ base_timer.py
@@ -0,0 +1,14 @@
+class BaseTimer():
+
+    def get_next_time() -> int:
+        """
+            return time interval after which some states will change
+        """
+        pass
+
+    def step(time_step):
+        """
+            change states accoring to the time step
+        """
+        pass
+
---
+++ simulate.py
@@ -0,0 +1,13 @@
+
+def main():
+    now_time = 0
+    while True:
+        """
+            get_next_time() from work_generator, worker, scheduler,
+            to find minimum next time
+        """
+        # xxx
+        """
+            step(min_time) for all Timers
+        """
+        # now_time += min_time
