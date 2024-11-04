0de142c09ddd423b10ffd02699595464a5b822ba
mtr329 <793562654@qq.com>
2024-11-04 18:19:30 +0800

    add self

--- base_timer.py
+++ base_timer.py
@@ -1,12 +1,12 @@
 class BaseTimer():
 
-    def get_next_time() -> int:
+    def get_next_time(self,) -> int:
         """
             return time interval after which some states will change
         """
         pass
 
-    def step(time_step):
+    def step(self, time_step):
         """
             change states accoring to the time step
         """
